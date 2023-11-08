1273f8503c53a5aaaeec392f5cd62f59cd9ef3a5
delasimensah <mensadelasi@gmail.com>
2023-11-08 21:20:43 +0000

    added messages api

--- components/modals/delete-message-modal.tsx
+++ components/modals/delete-message-modal.tsx
@@ -1,17 +1,46 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import qs from "query-string";
+import axios from "axios";
 
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
 import { useModal } from "@/hooks/use-modal-store";
 
 const DeleteMessageModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
 
   const isModalOpen = isOpen && type === "deleteMessage";
+  const { apiUrl, query } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const url = qs.stringifyUrl({
+        url: apiUrl || "",
+        query,
+      });
+
+      await axios.delete(url);
+
+      onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -20,7 +49,25 @@ const DeleteMessageModal = () => {
           <DialogTitle className="text-center text-2xl font-bold">
             Delete Message
           </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to do this? <br />
+            The message will be permanently deleted.
+          </DialogDescription>
         </DialogHeader>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex w-full items-center justify-between">
+            <Button disabled={isLoading} onClick={onClose} variant="ghost">
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              variant="primary"
+              onClick={handleClick}
+            >
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
