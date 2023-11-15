547b14013b5f50ff5aedb85a6a1c57723a49ad94
delasimensah <mensadelasi@gmail.com>
2023-11-15 11:47:02 +0000

    finished chat messages

--- components/chat/chat-welcome.tsx
+++ components/chat/chat-welcome.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { FC } from "react";
+import { Hash } from "lucide-react";
 
-const ChatWelcome = () => {
-  return <div>ChatWelcome</div>;
+type ChatWelcomeProps = {
+  name: string;
+  type: "channel" | "conversation";
+};
+
+const ChatWelcome: FC<ChatWelcomeProps> = ({ name, type }) => {
+  return (
+    <div className="mb-4 space-y-2 px-4">
+      {type === "channel" && (
+        <div className="flex h-[75px] w-[75px] items-center justify-center rounded-full bg-zinc-500 dark:bg-zinc-700">
+          <Hash className="h-12 w-12 text-white" />
+        </div>
+      )}
+      <p className="text-xl font-bold md:text-3xl">
+        {type === "channel" ? "Welcome to #" : ""}
+        {name}
+      </p>
+      <p className="text-sm text-zinc-600 dark:text-zinc-400">
+        {type === "channel"
+          ? `This is the start of the #${name} channel.`
+          : `This is the start of your conversation with ${name}`}
+      </p>
+    </div>
+  );
 };
 
 export default ChatWelcome;
