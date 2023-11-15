9f180d2711fd421273b32519b8a2b8a2a38d3d59
delasimensah <mensadelasi@gmail.com>
2023-11-15 11:54:50 +0000

    finished media room component

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+"use client";
 
-const MediaRoom = () => {
-  return <div>MediaRoom</div>;
+import { useEffect, useState, FC } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+type MediaRoomProps = {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+};
+
+const MediaRoom: FC<MediaRoomProps> = ({ chatId, video, audio }) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if (!user?.firstName || !user?.lastName) return;
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    (async () => {
+      try {
+        const resp = await fetch(
+          `/api/livekit?room=${chatId}&username=${name}`,
+        );
+
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, [user?.firstName, user?.lastName, chatId]);
+
+  if (token === "") {
+    return (
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <Loader2 className="my-4 h-7 w-7 animate-spin text-zinc-500" />
+
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={video}
+      audio={audio}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  );
 };
 
 export default MediaRoom;
